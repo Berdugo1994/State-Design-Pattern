b5dc6b46c70df5d61b183c2b5da2eb0f6aeaf11a
Eden Berdugo <62346207+Berdugo1994@users.noreply.github.com>
2021-01-11 22:31:52 +0200

    Delete src directory

--- src/com/company/State.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface State {
-    public void entry();
-    public void exit();
-    public void tm(int x);
-    public void TempChanged(double r, double c);
-
-}
